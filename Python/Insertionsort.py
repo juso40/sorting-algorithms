d605aa31021d809844f99082251f2e55f5322ebb
juso40 <justin.sostmann@googlemal.com>
2019-08-01 20:57:23 +0200

    Insertion and Selectionsort

---
+++ Python/Insertionsort.py
@@ -0,0 +1,33 @@
+"""
+Insertionsort
+We iterate over all elements and place them in the correct new order.
+We create the gaps for the insertions by shifting the elements one to the right.
+Worst case runtime O(n^2)
+"""
+
+
+def insertion_sort(arr):
+    n = len(arr)
+    for i in range(1, n):
+        elem = arr[i]  # remember our current element
+        j = i  # we guess that our element is already in the correct place
+        while j > 0 and arr[j - 1] > elem:  # Move greater elements than our current value one position ahead
+            arr[j] = arr[j - 1]
+            j -= 1
+        arr[j] = elem
+    return arr
+
+
+if __name__ == "__main__":
+    test_arr = [
+        3,
+        1,
+        7,
+        4,
+        12,
+        9,
+        11,
+        102
+    ]
+
+    print(insertion_sort(test_arr))
---
+++ Python/Selectionsort.py
@@ -0,0 +1,30 @@
+"""
+Selectionsort
+We search for the minimum element and place at the beginning of the unsorted part of the list.
+Worst case runtime O(n^2)
+"""
+
+
+def selection_sort(arr):
+    for i in range(len(arr)):
+        min_idx = i
+        for j in range(min_idx + 1, len(arr)):  # remaining unsorted list
+            if arr[min_idx] > arr[j]:
+                min_idx = j
+        arr[i], arr[min_idx] = arr[min_idx], arr[i]  # swap found minimal with the first element
+    return arr
+
+
+if __name__ == "__main__":
+    test_arr = [
+        3,
+        1,
+        7,
+        4,
+        12,
+        9,
+        11,
+        102
+    ]
+
+    print(selection_sort(test_arr))
